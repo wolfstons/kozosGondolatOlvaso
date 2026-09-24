237d7005590455ffb14138730d156bfc8ce4db27
wolfstons <paczi.balazs@gmail.com>
2026-09-24 13:00:13 +0200

    pakli kész

--- src/app/Pakli.java
+++ src/app/Pakli.java
@@ -1,13 +1,50 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package app;
 
-/**
- *
- * @author paczi.balazs
- */
+import java.util.Random;
+
 public class Pakli {
-    
+    private Lap[] lapok = new Lap[21];
+    private int db = 0;
+
+    public Pakli() {
+        feltolt();
+    }
+
+    public Lap[] getLapok() {
+        return lapok;
+    }
+
+    public void feltolt() {
+        String[] szinek = {"Piros", "Zold", "Tok"};
+        String[] ertekek = {"VII", "VIII", "IX", "X", "Also", "Felso", "Kiraly"};
+
+        db = 0;
+
+        for (String szin : szinek) {
+            for (String ertek : ertekek) {
+                lapok[db++] = new Lap(szin, ertek);
+            }
+        }
+    }
+
+    public void kever(int hanyszor) {
+        Random rnd = new Random();
+
+        for (int i = 0; i < hanyszor; i++) {
+            int a = rnd.nextInt(lapok.length);
+            int b = rnd.nextInt(lapok.length);
+
+            Lap seged = lapok[a];
+            lapok[a] = lapok[b];
+            lapok[b] = seged;
+        }
+    }
+
+    public Lap ezVolt() {
+        if (db > 0) {
+            return lapok[--db];
+        }
+        return null;
+    }
 }
